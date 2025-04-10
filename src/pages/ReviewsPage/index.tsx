6cd60438616dd407a4c6198afd68bcfc71888d43
Rinat Kov <frommrinat@gmail.com>
2025-04-10 18:51:39 +0200

    fix: corrected text, updated sections, and made other adjustments

--- src/pages/ReviewsPage/index.tsx
+++ src/pages/ReviewsPage/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-// import Reviews from "../components/Reviews";
+
 
 const ReviewsPage: React.FC = () => {
     return (
         <div>
             <h1>Отзывы</h1>
-            {/* <Reviews /> */}
         </div>
     );
 };
 
-export default ReviewsPage;w
+export default ReviewsPage;
